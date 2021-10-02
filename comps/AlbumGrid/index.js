974da544713286fecd7c174cd688f7ad23478908
Julian Mayes <juliantmayes@gmail.com>
2021-10-01 22:44:37 -0700

    checklist page

--- comps/AlbumGrid/index.js
+++ comps/AlbumGrid/index.js
@@ -2,7 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 import { useState } from "react";
-import { Star, Facebook, Mail, X, Menu as MenuIcon, ShoppingCart } from "react-feather";
+import {
+  Star,
+  Facebook,
+  Mail,
+  X,
+  Menu as MenuIcon,
+  ShoppingCart,
+  Spotify
+} from "react-feather";
+import ReactStars from "react-rating-stars-component";
+import {albums} from "@/data/albums"
 
 
 const ContainerUI = styled.div`
@@ -13,62 +23,71 @@ const ContainerUI = styled.div`
 `;
 
 const GridUI = styled.div`
-  width: 60%;
+  width: 50%;
   min-height: 100px;
   grid-template-columns: 1fr 1fr 1fr;
   display: grid;
   grid-gap: 50px;
-
 `;
 
-
 const AlbumUI = styled.div`
-    display: flex;
-  background: red;
+  display: flex;
   flex-direction: column;
+`;
+
+const AlbumImageUI = styled.img`
+cursor: pointer;
+transition: 0.2s ease;
+&:hover{
+    box-shadow: 4px 4px 10px grey;
+}
 
 `;
 
 const TitleUI = styled.div`
+font-size: 18px;
+font-weight: 700;
+margin: 10px 0 0 0;
+`;
 
+
+const ArtistUI = styled.div`
+    font-size: 16px;
+    margin: 5px 0 0 0;
 
 `;
 
 const ReviewUI = styled.div`
-
-    display: flex;
+  display: flex;
+  margin: 5px 0 0 0;
 `;
 
 
-
-
-const albums = [
-    {
-        "name" : "Abbey Road",
-        "artist" : "The Beatles",
-        "image" : "/abbey-road.png",
-        "rating" : "5"
-    },
-    
-]
-
-
 export default function AlbumGrid() {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
   return (
     <ContainerUI>
       <GridUI>
-
-          <AlbumUI>
-            <img width="100%"  src="abbey-road.png"/>
-            <TitleUI>Abbey Road</TitleUI>
-            <ReviewUI>
-               { <Star/> }
-           
-            </ReviewUI>
-          </AlbumUI>
-
-
-
+        {albums.map((album) => {
+          return (
+            <AlbumUI>
+              <AlbumImageUI width="100%" src={album.image} style={{filter: album.listened ? "grayscale(0)" : "grayscale(100)"}} />
+              <TitleUI>{album.name}</TitleUI>
+              <ArtistUI>{album.artist}</ArtistUI>
+              <ReviewUI>
+                <ReactStars
+                  count={album.rating}
+                  size={24}
+                  activeColor="#ffd700"
+                  onChange={ratingChanged}
+                />
+              </ReviewUI>
+            </AlbumUI>
+          );
+        })}
       </GridUI>
     </ContainerUI>
   );
---
+++ pages/checklist.js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+import { useState } from "react";
+import AlbumGrid from "@/comps/AlbumGrid";
+import { albums } from "@/data/albums";
+
+const ContainerUI = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+`;
+
+const TopUI = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 100px 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderUI = styled.div`
+  font-size: 36px;
+  margin: 10px 0;
+`;
+
+const NumberUI = styled.div`
+  font-size: 24px;
+  margin: 10px 0;
+`;
+
+const BackgroundUI = styled.img`
+  position: fixed;
+  top: 0px;
+  left: 0;
+  z-index: -100;
+`;
+const OverlayUI = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0;
+  z-index: -50;
+  width: 100%;
+  height: 100vh;
+  background: black;
+  opacity: 90%;
+`;
+
+export default function AlbumList() {
+  const [genre, setGenre] = useState("all");
+
+  function numberOfTrueValues(obj) {
+    var counter = 0;
+    for (var index = 0; index < obj.length; index++) {
+      if (obj[index].listened === true) {
+        counter++;
+      }
+    }
+    return counter;
+  }
+
+  return (
+    <ContainerUI>
+      <BackgroundUI src={"store-bg.png"} />
+      <OverlayUI />
+
+      <TopUI>
+        <HeaderUI>Albums Listened</HeaderUI>
+
+        <NumberUI>
+          {numberOfTrueValues(albums)} / {albums.length}
+        </NumberUI>
+      </TopUI>
+
+      <AlbumGrid />
+    </ContainerUI>
+  );
+}
