6a6c9ced7e966e0c32ab4bc8b50b426d9fdc578b
saihajvir <sgill433@my.bcit.ca>
2021-10-05 18:03:28 -0700

    fixed props, saihaj is stupid :)

--- comps/MainButton/index.js
+++ comps/MainButton/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { useState } from "react";
 import React from "react";
 
-const Button = styled.div`
+const ButtonUI = styled.div`
 
     display: flex;
     justify-content: center;
@@ -16,19 +16,21 @@ const Button = styled.div`
     font-family: Arial, Helvetica, sans-serif;
     font-weight: 600;
 
-    color: ${props=>props.txtCol};
-    background-color: ${props=>props.btnCol};
+    color: ${props=>props.color};
+    background-color: ${props=>props.backgroundColor};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `
 
-export default function ButtonUI(
-    txtCol='black',
-    btnCol='black',
-    btnText='default'
-){
+export const Button = ({
+    color='black',
+    backgroundColor='black',
+    buttonText='default'
+}) => {
     return (
-        <Button>
-            {btnText}
-        </Button>
+        <ButtonUI color={color} backgroundColor={backgroundColor}>
+            {buttonText}
+        </ButtonUI>
     )
 }
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,11 @@ import Image from "next/image";
 import styled from "styled-components";
 import { useState } from "react";
 import AlbumGrid from "@/comps/AlbumGrid";
-import ButtonUI from "@/comps/MainButton";
+import Button from "@/comps/MainButton";
 
 export default function Home() {
 
   return (
-    <>
-    
-    </>
+    <></>
   );
 }
